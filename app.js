7e7da1093cf2997537ceca2436644ec2f52c0e32
Adam Brailove <adam.brailove@gmail.com>
2021-10-26 21:58:28 -0700

    Add and test square and triangle wave generators

--- app.js
+++ app.js
@@ -1,69 +1,135 @@
-let iterationCount = 0;
-let interval_ms = 1;
-const start = Date.now();
-
 class SignalGenerator {
-  startTime = Date.now() / 1000; // Time at creation in seconds
-
-  calcPhaseOffsetSeconds(offsetRadians) {
-    return (this.phaseOffsetRadians / (2 * Math.PI)) * this.period;
+  calcPhaseShiftSec(phaseShiftRad) {
+    return (this.phaseShiftRad / (2 * Math.PI)) * this.period;
   }
 
-  constructor(period, phaseOffsetRadians, amplitude, amplitudeOffset) {
+  constructor(
+    startTime,
+    period,
+    dutyFactor,
+    phaseShiftRad,
+    amplitude,
+    amplitudeOffset
+  ) {
+    this.startTime = startTime;
     this.period = period; // in seconds
-    this.phaseOffsetRadians = phaseOffsetRadians; // store input value in radians
+    this.dutyFactor = dutyFactor; //0.0 to 1.0
+    this.phaseShiftRad = phaseShiftRad; // store input value in radians
     this.amplitude = amplitude; // unitless
     this.amplitudeOffset = amplitudeOffset; // unitless
 
-    this.phaseOffsetSeconds = this.calcPhaseOffsetSeconds(
-      this.phaseOffsetRadians
-    ); // Calc and cache phase offset in seconds
+    this.phaseShiftSec = this.calcPhaseShiftSec(this.phaseShiftRad); // Calc and cache phase offset in seconds
   }
 
-  // Method
-
-  phase() {
+  // Methods
+  phase(time) {
+    // Returns the phase in radians
     return (
       2 *
       Math.PI *
-      ((Date.now() / 1000 - this.startTime + this.phaseOffsetSeconds) % // Is sign of phase offset correct??
+      ((time - this.startTime + this.phaseShiftSec) % // Is sign of phase offset correct??
         this.period)
     );
   }
 
-  signal() {
+  signal(time) {
     return 0;
-  }
-
-  report() {
-    console.log(JSON.stringify(this));
   }
 }
 
 class SineGenerator extends SignalGenerator {
-  constructor(period, phaseOffset, amplitude, amplitudeOffset) {
-    super(period, phaseOffset, amplitude, amplitudeOffset); // call the super class constructor
+  constructor(startTime, period, phaseShiftRad, amplitude, amplitudeOffset) {
+    super(
+      startTime,
+      period,
+      0, // DutyFactor not used
+      phaseShiftRad,
+      amplitude,
+      amplitudeOffset
+    ); // call the super class constructor
   }
 
   // Methods
-  signal() {
-    return this.amplitudeOffset + this.amplitude * Math.sin(this.phase());
+  signal(time) {
+    return this.amplitudeOffset + this.amplitude * Math.sin(this.phase(time));
   }
 }
 
-// === Testing ============
-let gen1 = new SineGenerator(1.0, 0, 10, 0);
-let gen2 = new SineGenerator(1.0, 0.628, 10, 0);
-let gen3 = new SineGenerator(1.0, -0.628, 10, 0);
+class SquarewaveGenerator extends SignalGenerator {
+  constructor(
+    startTime,
+    period,
+    dutyFactor,
+    phaseShiftRad,
+    amplitude,
+    amplitudeOffset
+  ) {
+    super(
+      startTime,
+      period,
+      dutyFactor, //0 to 1.0
+      phaseShiftRad,
+      amplitude,
+      amplitudeOffset
+    ); // call the super class constructor
+  }
 
-gen1.report();
-gen2.report();
-gen3.report();
+  // Methods
+  signal(time) {
+    let phase = this.phase(time);
+    let positivePhase = phase >= 0.0 ? phase : phase + 2 * math.PI;
+    let sig = positivePhase <= 2 * Math.PI * this.dutyFactor ? 1.0 : 0.0;
+    return this.amplitudeOffset + this.amplitude * sig;
+  }
+}
+
+class TrianglewaveGenerator extends SignalGenerator {
+  constructor(
+    startTime,
+    period,
+    dutyFactor,
+    phaseShiftRad,
+    amplitude,
+    amplitudeOffset
+  ) {
+    super(
+      startTime,
+      period,
+      dutyFactor, //0 to 1.0
+      phaseShiftRad,
+      amplitude,
+      amplitudeOffset
+    ); // call the super class constructor
+  }
+
+  // Methods
+  signal(time) {
+    let twopi = 2 * Math.PI;
+    let phase = this.phase(time);
+    let positivePhase = phase >= 0.0 ? phase : phase + twopi;
+    let dutyFactorRad = twopi * this.dutyFactor;
+    let sig =
+      positivePhase <= dutyFactorRad
+        ? positivePhase / dutyFactorRad
+        : (twopi - positivePhase) / (twopi - dutyFactorRad);
+    return this.amplitudeOffset + this.amplitude * sig;
+  }
+}
+// === Testing ============
+
+let interval_ms = 5;
+const startTime = Date.now() / 1000; //Common start time for all generators, in seconds
+
+let gen1 = new TrianglewaveGenerator(startTime, 1.0, 0.5, 0, 10, 0);
+let gen2 = new TrianglewaveGenerator(startTime, 1.0, 0.0, 0, 10, 2);
+let gen3 = new TrianglewaveGenerator(startTime, 1.0, 1.0, 0, 10, -2);
 
 setInterval(() => {
+  let currentTime = Date.now() / 1000;
+  let elapsedTime = currentTime - startTime;
   console.log(
-    `${
-      (Date.now() - start) / 1000
-    } ${gen1.phase()} ${gen2.phase()} ${gen3.phase()}`
+    `${elapsedTime} ${gen1.signal(currentTime)} ${gen2.signal(
+      currentTime
+    )} ${gen3.signal(currentTime)}`
   );
-}, 10);
+}, interval_ms);
