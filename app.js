5d0eeea222890985422672f3b273226b2ffb4b00
Adam Brailove <adam.brailove@gmail.com>
2021-10-27 20:41:49 -0700

    re-implemented quasi-real-time setInterval loop

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ import {
   SquarewaveGenerator,
 } from "./signal-generator.mjs";
 
-let gen1 = new SinewaveGenerator(0, 2.3, 0, 10.0, 0);
-let gen2 = new SquarewaveGenerator(0, 1, 0.1, 0, 5.0, 0);
+const dt = 0.02; //seconds
+let t0 = Date.now() / 1000; //seconds
+let gen1 = new SinewaveGenerator(t0, 2.3, 0, 10.0, 0);
+let gen2 = new SquarewaveGenerator(t0, 1, 0.1, 0, 5.0, 0);
 
 console.log(`GEN1: ${JSON.stringify(gen1)}`);
 
-const dt = 0.02;
-for (let t = 0; t < 6; t += dt) {
+let t = t0;
+setInterval(() => {
   let s1 = gen1.signal(t);
   let s2 = gen2.signal(t);
-  console.log(`${t} ${s1} ${s2} ${s1 + s2}`);
-}
+  console.log(`${t - t0} ${s1} ${s2} ${s1 + s2}`);
+  t = Date.now() / 1000;
+}, dt * 1000);
