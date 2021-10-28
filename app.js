91a7f93c6f316058aeaa0bad50a6b672bcaf8c72
Adam Brailove <adam.brailove@gmail.com>
2021-10-28 11:46:26 -0700

    Added .env

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import { InfluxDB } from "@influxdata/influxdb-client";
+import dotenv from "dotenv";
+dotenv.config();
+
 import {
   SinewaveGenerator,
   TrianglewaveGenerator,
@@ -9,7 +13,14 @@ let t0 = Date.now() / 1000; //seconds
 let gen1 = new SinewaveGenerator(t0, 2.3, 0, 10.0, 0);
 let gen2 = new SquarewaveGenerator(t0, 1, 0.1, 0, 5.0, 0);
 
-console.log(`GEN1: ${JSON.stringify(gen1)}`);
+// Influx client init
+const token = process.env.INFLUXDB_TOKEN;
+const org = "adam.brailove@gmail.com";
+const bucket = "SignalGeneratorTestBucket";
+const client = new InfluxDB({
+  url: "https://us-west-2-1.aws.cloud2.influxdata.com",
+  token: token,
+});
 
 let t = t0;
 setInterval(() => {
