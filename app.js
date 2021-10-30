9d67d3480ba119be3ced766ed9f694367a7791fc
Adam Brailove <adam.brailove@gmail.com>
2021-10-30 14:41:23 -0700

    DB writes done inside setInterval()

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import {
   SquarewaveGenerator,
 } from "./signal-generator.mjs";
 
-// Influx client init
+// Init Influx client
 const token = process.env.INFLUXDB_TOKEN;
 const org = process.env.INFLUXDB_ORG;
 const bucket = process.env.INFLUXDB_BUCKET;
@@ -18,34 +18,42 @@ const client = new InfluxDB({
   token: token,
 });
 const writeApi = client.getWriteApi(org, bucket);
-writeApi.useDefaultTags({ timer: "freerun" });
+writeApi.useDefaultTags({});
 
 // Init time and signal sources
 const t0 = Date.now() / 1000; // Start time, seconds
 let t = t0;
+let measurementCount = 0;
 let gen1 = new SinewaveGenerator(t0, 2.3, 0, 10.0, 0);
 let gen2 = new SquarewaveGenerator(t0, 1, 0.1, 0, 5.0, 0);
 
 //Write points loop
-for (let i = 0; i < 20; i++) {
-  let s1 = gen1.signal(t);
-  let s2 = gen2.signal(t);
-  let elapsed = t - t0;
-  console.log(`${elapsed} ${s1} ${s2}`);
-
-  const point = new Point("mem").floatField("index", i);
-  writeApi.writePoint(point);
-
-  t = Date.now() / 1000; //Update time, seconds
-}
-
-//Flush and close
-writeApi
-  .close()
-  .then(() => {
-    console.log("FINISHED");
-  })
-  .catch((e) => {
-    console.error(e);
-    console.log("Finished ERROR");
-  });
+let intervalHandle = setInterval(() => {
+  if (measurementCount <= 200) {
+    let s1 = gen1.signal(t);
+    let s2 = gen2.signal(t);
+    let elapsedTime = t - t0;
+    console.log(`${measurementCount} ${elapsedTime} ${s1} ${s2} ${s1 + s2}`);
+    const point = new Point("testData2")
+      .floatField("measurementCount", measurementCount)
+      .floatField("elapsedTime", elapsedTime)
+      .floatField("s1", s1)
+      .floatField("s2", s2)
+      .floatField("s1+s2", s1 + s2);
+    writeApi.writePoint(point);
+    t = Date.now() / 1000; //Update time, second
+    measurementCount++;
+  } else {
+    //Flush and close
+    writeApi
+      .close()
+      .then(() => {
+        console.log("FINISHED");
+      })
+      .catch((e) => {
+        console.error(e);
+        console.log("Finished ERROR");
+      });
+    clearInterval(intervalHandle);
+  }
+}, 10);
