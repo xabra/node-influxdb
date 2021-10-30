8c600a1e181aea31adcda6938566e0aa871255da
Adam Brailove <adam.brailove@gmail.com>
2021-10-29 21:54:14 -0700

    simplified

--- app.js
+++ app.js
@@ -9,45 +9,43 @@ import {
   SquarewaveGenerator,
 } from "./signal-generator.mjs";
 
-const dt = 8; //seconds
-let t0 = Date.now() / 1000; //seconds
+// Influx client init
+const token = process.env.INFLUXDB_TOKEN;
+const org = process.env.INFLUXDB_ORG;
+const bucket = process.env.INFLUXDB_BUCKET;
+const client = new InfluxDB({
+  url: INFLUXDB_URL,
+  token: token,
+});
+const writeApi = client.getWriteApi(org, bucket);
+writeApi.useDefaultTags({ timer: "freerun" });
+
+// Init time and signal sources
+const t0 = Date.now() / 1000; // Start time, seconds
+let t = t0;
 let gen1 = new SinewaveGenerator(t0, 2.3, 0, 10.0, 0);
 let gen2 = new SquarewaveGenerator(t0, 1, 0.1, 0, 5.0, 0);
 
-// Influx client init
-const token = process.env.INFLUXDB_TOKEN;
-const org = "adam.brailove@gmail.com";
-const bucket = "SignalGeneratorTestBucket";
-const client = new InfluxDB({
-  url: "https://us-west-2-1.aws.cloud2.influxdata.com",
-  token: token,
-});
-
-const writeApi = client.getWriteApi(org, bucket);
-writeApi.useDefaultTags({ host: "host1" });
-
-let t = t0;
-const intervalTask = setInterval(() => {
+//Write points loop
+for (let i = 0; i < 20; i++) {
   let s1 = gen1.signal(t);
   let s2 = gen2.signal(t);
   let elapsed = t - t0;
   console.log(`${elapsed} ${s1} ${s2} ${s1 + s2}`);
 
-  const point = new Point("mem").floatField("Elapsed2", elapsed);
+  const point = new Point("mem").floatField("Elapsed3", elapsed);
   writeApi.writePoint(point);
 
-  t = Date.now() / 1000;
-}, dt * 1000);
+  t = Date.now() / 1000; //Update time, seconds
+}
 
-setTimeout(() => {
-  clearInterval(intervalTask);
-  writeApi
-    .close()
-    .then(() => {
-      console.log("FINISHED");
-    })
-    .catch((e) => {
-      console.error(e);
-      console.log("Finished ERROR");
-    });
-}, 100000);
+//Flush and close
+writeApi
+  .close()
+  .then(() => {
+    console.log("FINISHED");
+  })
+  .catch((e) => {
+    console.error(e);
+    console.log("Finished ERROR");
+  });
